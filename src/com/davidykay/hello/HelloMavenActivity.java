ebae0fe3f7d3da4c73490b637456b9dc2c08acfa
David Young-Chan Kay <davidykay@gmail.com>
2011-12-27 20:43:52 -0500

    Barebones Maven Android Project.

---
+++ src/com/davidykay/hello/HelloMavenActivity.java
@@ -0,0 +1,13 @@
+package com.davidykay.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloMavenActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
